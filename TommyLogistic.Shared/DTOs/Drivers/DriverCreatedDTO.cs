92cd0f2dec6642f3643a167c680a92f06f656996
Tomas Gonzales Guerra <tommy95pro@outlook.com>
2026-05-06 02:22:47 -0500

    DriverCreatedDTO

--- TommyLogistic.Shared/DTOs/Drivers/DriverCreatedDTO.cs
+++ TommyLogistic.Shared/DTOs/Drivers/DriverCreatedDTO.cs
@@ -1,14 +1,11 @@
 ﻿namespace TommyLogistic.Shared.DTOs.Drivers;
 
-public class DriverDTO
+public class DriverCreatedDTO
 {
-    public int Id { get; set; }
     public string? DNI { get; set; }
     public string? Placa { get; set; }
     public string? Photo { get; set; }
     public bool Available { get; set; }
     public string? Celular { get; set; }
     public string? FullName { get; set; }
-    public int DeliveredToday { get; set; }
-    public int ActiveOrderToday { get; set; }
 }
