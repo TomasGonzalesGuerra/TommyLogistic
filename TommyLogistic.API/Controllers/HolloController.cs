b8af0df89e7f7c2a4689a374423ae949c0e374df
pc <pc@ANTAREZ-01>
2026-06-08 12:01:09 -0500

    Publicar es un Dolor de 🥚🥚

---
+++ TommyLogistic.API/Controllers/HolloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TommyLogistic.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HolloController : ControllerBase
+{
+    // GET: api/Hollo/hi
+    [HttpGet("hi")]
+    public async Task<ActionResult<string>> GetAdminDashboardAsync()
+    {
+        Console.WriteLine("Hola desde el Api  en el Server");
+        return Ok($"Hola soy Yop");
+    }
+}
--- TommyLogistic.Web/Program.cs
+++ TommyLogistic.Web/Program.cs
@@ -18,8 +18,6 @@ builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<LogisticWebProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider, LogisticWebProvider>(x => x.GetRequiredService<LogisticWebProvider>());
 builder.Services.AddScoped<ILoginService, LogisticWebProvider>(x => x.GetRequiredService<LogisticWebProvider>());
-var authProvider = builder.Build().Services.GetRequiredService<LogisticWebProvider>();
-await authProvider.InitializeAsync();
 // ── Servicios ──────────────────────────────────────────────────────────────
 builder.Services.AddSweetAlert2();
 builder.Services.AddBlazoredLocalStorage();
@@ -32,5 +30,9 @@ builder.Services.AddScoped<OperatorService>();
 builder.Services.AddSingleton<ComingSoonService>();
 builder.Services.AddSingleton<NotificationService>();
 
+//var host = builder.Build();
+//var authProvider = host.Services.GetRequiredService<LogisticWebProvider>();
+//await authProvider.InitializeAsync();
+//await host.RunAsync();
 
 await builder.Build().RunAsync();
