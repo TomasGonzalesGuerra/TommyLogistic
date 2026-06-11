4ace1f0574184b85b77fb0eb616bb874ba43e76c
pc <pc@ANTAREZ-01>
2026-06-11 11:59:03 -0500

    ConnectionStrings

--- TommyLogistic.API/Program.cs
+++ TommyLogistic.API/Program.cs
@@ -15,7 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Services
 builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
-builder.Services.AddDbContext<LogisticDataContext>(dc => dc.UseSqlServer(builder.Configuration["ConnectionStrings:AzureDBConnection"]));
+builder.Services.AddDbContext<LogisticDataContext>(dc => dc.UseSqlServer(builder.Configuration["ConnectionStrings:SomeeDBConnection"]));
 builder.Services.AddTransient<SeedDb>();
 
 builder.Services.AddIdentity<User, IdentityRole>(x =>
